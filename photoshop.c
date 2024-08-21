a99b30ed601010c7d3d10cc1c5736487843d8a99
Vinicius Castro <viniciuscastro0711@gmail.com>
2024-08-20 23:00:39 -0300

    feat: Add convolution function

--- photoshop.c
+++ photoshop.c
@@ -15,22 +15,44 @@ typedef struct PGMimg {
 
 int readFile(PGMimg* pgm, char* filename);
 int writeFile(PGMimg* pgm, char* filename);
+int transferData(PGMimg* in, PGMimg* out);
+int convolution(PGMimg* in, PGMimg* out, int** kernel, int radius, int totalWeight);
 
-void teste(PGMimg* pgm){
+//Função temporária pra testes
+void teste(){
+    PGMimg* a = (PGMimg*)malloc(sizeof(PGMimg));
     char name[100];
     scanf("%s", name);
-    if (readFile(pgm, name) == 1) {
+    if (readFile(a, name) == 1) {
+        PGMimg* b = (PGMimg*)malloc(sizeof(PGMimg));
+        transferData(a, b);
         printf("Qual o nome do output?\n");
         char outName[100];
         scanf("%s", outName);
-        writeFile(pgm, outName);
+        //GAMBIARRA ABSURDA!!!!!
+        int** gauss = (int**)malloc(3*sizeof(int*));
+        int i;
+        for (i = 0; i < 3; i++){
+            gauss[i] = (int*)malloc(3*sizeof(int));
+        }
+        gauss[0][0] = 1;
+        gauss[0][1] = 2;
+        gauss[0][2] = 1;
+        gauss[1][0] = 2;
+        gauss[1][1] = 4;
+        gauss[1][2] = 2;
+        gauss[2][0] = 1;
+        gauss[2][1] = 2;
+        gauss[2][2] = 1;
+        
+        convolution(a, b, gauss, 1, 16);
+        writeFile(b, outName);
     }
 }
 
 int main(){
     setlocale(LC_ALL, "Portuguese");
-    PGMimg* pgm = (PGMimg*)malloc(sizeof(PGMimg));
-    teste(pgm);
+    teste();
 
     return 0;
 }
@@ -55,31 +77,32 @@ int readFile(PGMimg* pgm, char* filename){
         return 0;
     }
 
-    //Lê o comentário, as dimensões e o Maxval do arquivo
+    //Lendo o comentário, as dimensões e o Maxval do arquivo
     fscanf(imgFile, " %80[^\n]s", pgm->com);
     fscanf(imgFile, "%d %d", &(pgm->width), &(pgm->height));
     fscanf(imgFile, "%d", &(pgm->maxVal));
 
-    //Checa se as dimensões do arquivo são válidas
+    //Checando se as dimensões do arquivo são válidas
     if (pgm->height <= 0 || pgm->width <= 0){
         printf("Arquivo inválido!\n");
         return 0;
     }
 
-    //Aloca memória para a imagem
+    //Alocando memória para a imagem
     pgm->pixels = (int**)malloc(pgm->height * sizeof(int*));
     if (pgm->pixels == NULL) {
-        printf("Malloc falhou1\n");
+        printf("Malloc falhou\n");
         return 0;
     }
     for (int i = 0; i < pgm->height; i++){
         pgm->pixels[i] = (int*)malloc(pgm->width * sizeof(int));
         if (pgm->pixels[i] == NULL) {
-            printf("Malloc falhou2\n");
+            printf("Malloc falhou\n");
             return 0;
         }
         //Lendo os pixels da imagem
-        for (int j = 0; j < pgm->width; j++){
+        int j;
+        for (j = 0; j < pgm->width; j++){
             fscanf(imgFile, "%d", &pgm->pixels[i][j]);
         }
     }
@@ -96,23 +119,92 @@ int writeFile(PGMimg* pgm, char* filename){
     FILE *imgFile = fopen(filename, "w+");
     printf("Arquivo lido\n");
     
-    //Escreve as informações no arquivo
+    //Escrevendo as informações no arquivo
     fprintf(imgFile, "%s\n", pgm->type);
     fprintf(imgFile, "%s\n", pgm->com);
     fprintf(imgFile, "%d %d\n", pgm->width, pgm->height);
     fprintf(imgFile, "%d\n", pgm->maxVal);
 
     //Coloca a informação da imagem no arquivo
-    for (int i = 0; i < pgm->height; i++){
-        for (int j = 0; j < pgm->width; j++){
-            if (j != 0) fprintf(imgFile, "%c", ' ');
+    int i, j;
+    for (i = 0; i < pgm->height; i++){
+        for (j = 0; j < pgm->width; j++){
+            if (j != 0) {
+                fprintf(imgFile, "%c", ' ');
+                if (j % 6 == 0) fprintf(imgFile, "%c", '\n'); //Não fica bonito mas funciona
+            }
             fprintf(imgFile, "%d", pgm->pixels[i][j]);
         }
         fprintf(imgFile, "%c", '\n');
     }
 
-    //Fecha o arquivo
+    //Fechando o arquivo
     fclose(imgFile);
+
+    return 1;
+}
+
+
+int transferData(PGMimg* in, PGMimg* out){
+    //Transfere as informações de uma imagem para outra
+    strcpy(out->type, in->type);
+    strcpy(out->com, in->com);
+    out->maxVal = in->maxVal;
+    out->width = in->width;
+    out->height = in->height;
+
+    out->pixels = (int**)malloc(out->height * sizeof(int*));
+    if (out->pixels == NULL) {
+        printf("Malloc falhou\n");
+        return 0;
+    }
+    for (int i = 0; i < out->height; i++){
+        out->pixels[i] = (int*)malloc(out->width * sizeof(int));
+        if (out->pixels[i] == NULL) {
+            printf("Malloc falhou\n");
+            return 0;
+        }
+        int j;
+        for (j = 0; j < out->width; j++){
+            out->pixels[i][j] = in->pixels[i][j];
+        }
+    }
+    printf("Informação transferida entre imagens!\n");
+
+    return 1;
+}
+
+int convolution(PGMimg* in, PGMimg* out, int** kernel, int radius, int totalWeight){
+    //Testando se as imagens tem o mesmo tamanho;
+    if (in->width != out->width || in->height != out->height){
+        printf("Algo deu errado na convolução\n");
+        return 0;
+    }
+    //Realizando a convolução no arquivo in e colocando os resultados no arquivo out
+    printf("Iniciando convolução\n");
+    int i, j, a, b;
+    for (i = 0; i < in->height; i++){
+        for (j = 0; j < in->width; j++){
+            int validPos = (radius * 2 + 1) * (radius * 2 + 1);
+            int weights = totalWeight;
+            int light = 0; //Oh nome ruim kkkkkk
+            for (a = -radius; a <= radius; a++){
+                for (b = -radius; b <= radius; b++){
+                    if ((i + a <= -1) || (i + a >= in->height) || (j + b <= -1) || (j + b >= in->width)){
+                        validPos--;
+                        weights -= kernel[radius+a][radius+b];
+                    }
+                    else {
+                        light+= (in->pixels)[i+a][j+b] * kernel[radius+a][radius+b];
+                    }
+                }
+            }
+            //Calculando o resultado de out[i][j]
+            out->pixels[i][j] = light/weights;
+        }
+        printf("Linha %d concluída\n", i);
+    }
+    printf("Convolução concluída\n");
 
     return 1;
 }
