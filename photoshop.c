87215a0108a977b1cf6eb3c9fb45bf0015e299b8
Vinicius Castro <viniciuscastro0711@gmail.com>
2024-08-23 23:03:53 -0300

    Kernels organizados, mas não lê mais as dimensões

--- photoshop.c
+++ photoshop.c
@@ -13,16 +13,116 @@ typedef struct PGMimg {
     int** pixels;
 } PGMimg;
 
+typedef struct kernel {
+    int** matrix;
+    int totalWeight;
+    int radius;
+    int positions;
+} kernel;
+
 int readFile(PGMimg* pgm, char* filename);
 int writeFile(PGMimg* pgm, char* filename);
 int transferData(PGMimg* in, PGMimg* out);
-int convolution(PGMimg* in, PGMimg* out, int** kernel, int radius, int totalWeight);
-int contaDigitos(int n);
+int convolution(PGMimg* in, PGMimg* out, kernel* k);
+int digits(int n);
+void createKernel(kernel* k, int n, int totalWeight, int radius, int positions);
 
 //Função temporária pra testes
 void teste(){
+    
+}
+
+int main(){
+    setlocale(LC_ALL, "Portuguese");
+    int i, j;
+    //Inicializando o kernel do filtro da mediana
+    kernel* median = (kernel*)malloc(sizeof(kernel));
+    int auxMedian[3][3] = {
+        {1, 1, 1}, 
+        {1, 1, 1}, 
+        {1, 1, 1}
+    };
+    createKernel(median, 3, 9, 2, 9);
+    for (i = 0; i < 3; i++){
+        for (j = 0; j < 3; j++){
+            median->matrix[i][j] = auxMedian[i][j];
+        }
+    }
+    //Inicializando o kernel do filtro gaussiano 3x3
+    kernel* gauss3 = (kernel*)malloc(sizeof(kernel));
+    int auxGauss3[3][3] = {
+        {1, 2, 1}, 
+        {2, 4, 2}, 
+        {1, 2, 1}
+    };
+    createKernel(gauss3, 3, 16, 2, 9);
+    for (i = 0; i < 3; i++){
+        for (j = 0; j < 3; j++){
+            gauss3->matrix[i][j] = auxGauss3[i][j];
+        }
+    }
+    //Inicializando o kernel do filtro gaussiano 5x5
+    kernel* gauss5 = (kernel*)malloc(sizeof(kernel));
+    int auxGauss5[5][5] = {
+        {1, 4, 7, 4, 1}, 
+        {4, 16, 26, 16, 4}, 
+        {7, 26, 41, 26, 7}, 
+        {4, 16, 26, 16, 4}, 
+        {1, 4, 7, 4, 1}
+    };
+    createKernel(gauss5, 5, 273, 3, 25);
+    for (i = 0; i < 5; i++){
+        for (j = 0; j < 5; j++){
+            gauss5->matrix[i][j] = auxGauss5[i][j];
+        }
+    }
+    //Inicializando o kernel do filtro gaussiano 7x7
+    kernel* gauss7 = (kernel*)malloc(sizeof(kernel));
+    int auxGauss7[7][7] = {
+        {0, 0, 1, 2, 1, 0, 0},
+        {0, 3, 13, 22, 13, 3, 0},
+        {1, 13, 59, 97, 59, 13, 1},
+        {2, 22, 97, 159, 97, 22, 2},
+        {1, 13, 59, 97, 59, 13, 1},
+        {0, 3, 13, 22, 13, 3, 0},
+        {0, 0, 1, 2, 1, 0, 0}
+    };
+    createKernel(gauss7, 7, 1003, 4, 49);
+    for (i = 0; i < 7; i++){
+        for (j = 0; j < 7; j++){
+            gauss7->matrix[i][j] = auxGauss7[i][j];
+        }
+    }
+    //Inicializando o kernel horizontal do filtro Sorbel
+    kernel* sobelX = (kernel*)malloc(sizeof(kernel));
+    int auxSobelX[3][3] = {
+        {1, 0, -1}, 
+        {2, 0, -2}, 
+        {1, 0, -1}
+    };
+    createKernel(sobelX, 3, 0, 2, 9);
+    for (i = 0; i < 3; i++){
+        for (j = 0; j < 3; j++){
+            sobelX->matrix[i][j] = auxSobelX[i][j];
+        }
+    }
+    //Inicializando o kernel vertical do filtro Sobel
+    kernel* sobelY = (kernel*)malloc(sizeof(kernel));
+    int auxSobelY[3][3] = {
+        {1, 2, 1}, 
+        {0, 0, 0}, 
+        {-1, -2, -1}
+    };
+    createKernel(sobelY, 3, 0, 2, 9);
+    for (i = 0; i < 3; i++){
+        for (j = 0; j < 3; j++){
+            sobelY->matrix[i][j] = auxSobelY[i][j];
+        }
+    }
+    
     PGMimg* a = (PGMimg*)malloc(sizeof(PGMimg));
     char name[100];
+    printf("Qual o nome do input?\n");
     scanf("%s", name);
     if (readFile(a, name) == 1) {
         PGMimg* b = (PGMimg*)malloc(sizeof(PGMimg));
@@ -30,80 +130,25 @@ void teste(){
         printf("Qual o nome do output?\n");
         char outName[100];
         scanf("%s", outName);
-        /*//GAMBIARRA ABSURDA!!!!!
-        int** gauss = (int**)malloc(3*sizeof(int*));
-        int i;
-        for (i = 0; i < 3; i++){
-            gauss[i] = (int*)malloc(3*sizeof(int));
-        }
-        gauss[0][0] = 1;
-        gauss[0][1] = 2;
-        gauss[0][2] = 1;
-        gauss[1][0] = 2;
-        gauss[1][1] = 4;
-        gauss[1][2] = 2;
-        gauss[2][0] = 1;
-        gauss[2][1] = 2;
-        gauss[2][2] = 1;
-        */
-
-        int** kernel;
-        int* rows;
-
-        printf("Escolha um Kernel: ");
-        printf("1.Gauss: \n{1, 2, 1\n2, 4, 2\n1, 2, 1\n}.\n");
-        printf("2.Entre com seu kernel.\n");
-        int op, dimention, i;
-        int** gauss;
+        int op;
+        printf("Qual o filtro? 1 = Mediana, 2 = Gauss Low, 3 = Gauss Mid, 4 = Gauss High\n");
         scanf("%d", &op);
-        switch(op){
-            case 1:
-                gauss = (int**)malloc(3*sizeof(int*));
-                for (i = 0; i < 3; i++){
-                    gauss[i] = (int*)malloc(3*sizeof(int));
-                }
-                gauss[0][0] = 1;
-                gauss[0][1] = 2;
-                gauss[0][2] = 1;
-                gauss[1][0] = 2;
-                gauss[1][1] = 4;
-                gauss[1][2] = 2;
-                gauss[2][0] = 1;
-                gauss[2][1] = 2;
-                gauss[2][2] = 1;
-                kernel = gauss;
-                break;
-            case 2:
-                printf("Digite a dimensao do seu Kernel: ");
-                scanf("%d", &dimention);
-                kernel = (int**)malloc(sizeof(int*) * dimention);
-                int row;
-                for(row = 0; row < dimention; row++){
-                    rows = (int*)malloc(sizeof(int) * dimention);
-                    int column;
-                    for(column = 0; column < dimention; column++){
-                        scanf("%d", &kernel[row][column]);
-                    }
-                }
-                break;
+        switch (op) {
+        case 1:
+            convolution(a, b, median);
+            break;
+        case 2:
+            convolution(a, b, gauss3);
+            break;
+        case 3:
+            convolution(a, b, gauss5);
+            break;
+        case 4:
+            convolution(a, b, gauss7);
+            break;
         }
-
-        convolution(a, b, kernel, 1, 16);
         writeFile(b, outName);
-
-        
-        int row;
-        for(row = 0; row < dimention; row++){
-            free(rows);
-        }
-        free(kernel);
-        
     }
-}
-
-int main(){
-    setlocale(LC_ALL, "Portuguese");
-    teste();
 
     return 0;
 }
@@ -124,7 +169,7 @@ int readFile(PGMimg* pgm, char* filename){
 
     //Se não for um plain PMG, recusa o arquivo
     if (strcmp(pgm->type, "P2")){
-        printf("Arquivo inválido!\n");
+        printf("Arquivo de tipo inválido!\n");
         return 0;
     }
 
@@ -139,7 +184,7 @@ int readFile(PGMimg* pgm, char* filename){
 
     //Checando se as dimensões do arquivo são válidas
     if (pgm->height <= 0 || pgm->width <= 0){
-        printf("Arquivo inválido!\n");
+        printf("Arquivo de dimensões inválidas!\n");
         return 0;
     }
 
@@ -168,7 +213,6 @@ int readFile(PGMimg* pgm, char* filename){
 
     return 1;
 }
-
 int writeFile(PGMimg* pgm, char* filename){
     //PS: Isso vai sobreescrever qualquer arquivo que tenha o nome recebido pela função
     //Abrindo o arquivo
@@ -186,7 +230,7 @@ int writeFile(PGMimg* pgm, char* filename){
     for (i = 0; i < pgm->height; i++){
         for (j = 0; j < pgm->width; j++){
             fprintf(imgFile, "%d", pgm->pixels[i][j]);
-            for(k = 0; k < 1 + (contaDigitos(pgm->maxVal) - contaDigitos(pgm->pixels[i][j])); k++){
+            for(k = 0; k < 1 + (digits(pgm->maxVal) - digits(pgm->pixels[i][j])); k++){
                 fputc(' ', imgFile);
             }
             nums++;
@@ -202,7 +246,6 @@ int writeFile(PGMimg* pgm, char* filename){
 
     return 1;
 }
-
 int transferData(PGMimg* in, PGMimg* out){
     //Transfere as informações de uma imagem para outra
     strcpy(out->type, in->type);
@@ -232,8 +275,7 @@ int transferData(PGMimg* in, PGMimg* out){
 
     return 1;
 }
-
-int convolution(PGMimg* in, PGMimg* out, int** kernel, int radius, int totalWeight){
+int convolution(PGMimg* in, PGMimg* out, kernel* k){
     //Testando se as imagens tem o mesmo tamanho;
     if (in->width != out->width || in->height != out->height){
         printf("Algo deu errado na convolução\n");
@@ -244,17 +286,17 @@ int convolution(PGMimg* in, PGMimg* out, int** kernel, int radius, int totalWeig
     int i, j, a, b;
     for (i = 0; i < in->height; i++){
         for (j = 0; j < in->width; j++){
-            int validPos = (radius * 2 + 1) * (radius * 2 + 1);
-            int weights = totalWeight;
+            int validPos = k->positions;
+            int weights = k->totalWeight;
             int light = 0; //Oh nome ruim kkkkkk
-            for (a = -radius; a <= radius; a++){
-                for (b = -radius; b <= radius; b++){
+            for (a = -k->radius; a <= k->radius; a++){
+                for (b = -k->radius; b <= k->radius; b++){
                     if ((i + a <= -1) || (i + a >= in->height) || (j + b <= -1) || (j + b >= in->width)){
                         validPos--;
-                        weights -= kernel[radius+a][radius+b];
+                        weights -= k->matrix[k->radius+a][k->radius+b];
                     }
                     else {
-                        light+= (in->pixels)[i+a][j+b] * kernel[radius+a][radius+b];
+                        light+= (in->pixels)[i+a][j+b] * k->matrix[k->radius+a][k->radius+b];
                     }
                 }
             }
@@ -267,8 +309,7 @@ int convolution(PGMimg* in, PGMimg* out, int** kernel, int radius, int totalWeig
 
     return 1;
 }
-
-int contaDigitos(int n){
+int digits(int n){
     if (n < 10) return 1;
     if (n < 100) return 2;
     if (n < 1000) return 3;
@@ -276,4 +317,14 @@ int contaDigitos(int n){
     if (n < 100000) return 5;
     printf("ERRO!!");
     return 0;
+}
+void createKernel(kernel* k, int n, int totalWeight, int radius, int positions){
+    k->totalWeight = totalWeight;
+    k->radius = radius;
+    k->positions = positions;
+    k->matrix = (int**)malloc(n*sizeof(int*));
+    int i;
+    for (i = 0; i < n; i++){
+        k->matrix[i] = (int*)malloc(n*sizeof(int));
+    }
 }
